4466256e4e904b94721d11e784dae18e90b89a8b
Ákos Putóczky <pootyx@gmail.com>
2020-02-22 14:08:36 +0100

    Add challenge controller

--- controllers/challengeControllers.go
+++ controllers/challengeControllers.go
@@ -1,20 +1,42 @@
 package controllers
 
 import (
+	"../repository"
+	u "../utils"
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
 	"net/http"
 )
 
 func GetChallenges(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Get Challenges!")
+	fmt.Fprintf(w, "Get Challenge!")
 }
 
 func CreateChallenge(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Create Challenge!")
+	var newChallenge repository.Challenge
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+	fmt.Println(reqBody)
+	json.Unmarshal(reqBody, &newChallenge)
+
+	newChallenge.Create()
+	fmt.Println(newChallenge)
 }
 
 func GetChallenge(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Get Challenge!")
+	vars := mux.Vars(r)
+	id := vars["uuid"]
+	data := repository.GetChallenge(id)
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
 }
 
 func GetChallengeBets(w http.ResponseWriter, r *http.Request) {
